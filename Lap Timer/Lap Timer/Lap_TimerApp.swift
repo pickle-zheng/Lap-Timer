08952d05383a76d8b8c7abd2506b524b5d7f7cca
Zheng Zhao <fixfish@gmail.com>
2022-02-14 19:32:22 +1100

    project setup

---
+++ Lap Timer/Lap Timer/Lap_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lap_TimerApp.swift
+//  Lap Timer
+//
+//  Created by Zheng on 14/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct Lap_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
